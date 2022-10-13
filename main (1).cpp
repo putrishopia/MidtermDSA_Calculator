7b1c6afddc34272c10b2b5e590e43c615cd8363a
putrishopia <115162531+putrishopia@users.noreply.github.com>
2022-10-13 20:55:49 +0700

    Update main (1).cpp

--- main (1).cpp
+++ main (1).cpp
@@ -7,64 +7,60 @@ using namespace std;
 
 int main ()
 {
-	cout<<"Scientific Calculator Made by Allister, Ben, Christoper, Nadhief, Shopia.\n";
-	cout<<"-------------------------------------------------------------------------\n";
-
+	cout<<"Scientific Calculator\n";
 	Calculator c1;  //object of Calculator
 	string form;    //reading formula in
 	string insertToHistory; //to insert into history
 	char var;       //reading variable
 	double value;   //reading variable value
 	float result;
-
+	
 	int OnOff=1;	//to activate loop and menu option. any random number but 0 could be used
 	Node* head = NULL;
-
+	
 	while (OnOff!=0){
-
-		cout<<"What would you want to do?\n";
+		cout<<"\nWhat would you want to do?\n";
 		cout<<"1: to make calculation\n";
 		cout<<"2: to see calculation history\n";
-		cout<<"3: Calculator instructions\n";
-		cout<<"0: shut down\n\n";
+		cout<<"0: shut down\n";
 		cin>>OnOff;
-
+		
 		//to do a calculation
 		if (OnOff==1){
 			cout<<"Please, enter your formula:";
 			cin>>form;
-
+		
 			Calculator userExpression(form);
 			if (userExpression.isLegal(form) == false)
     			continue;
     		else{
         	    double result = userExpression.eval();
-
+        	    
         	    string calcResult=to_string(result);
 				string equal="=";
-				insertToHistory=form+equal+calcResult;
+				insertToHistory=form+equal+calcResult;        	    
     			cout << "\nResult: " <<  result << "\n\n";
     			push(&head, insertToHistory);
     		}
-
+						
 			OnOff=3;
 		}
-
+		
 		//to look at history
 		else if(OnOff==2){
 			int historyOperation=3;
 			while (historyOperation!=0){
 				cout<<"\nYour calculation history:\n";
 				printHistory(head);
-				cout<<"\nWhat do you want to do?\n"
+				cout<<"\nwhat do you want to do?\n"
 					<<"1: to remove a history\n"
-					<<"0: to exit history\n";
+					<<"0: to exit history\n";	
 				cin>>historyOperation;
 				//delete history
 				if(historyOperation==1){
     				cout<<"\ninsert what to delete? (copy the equation to the command, no 0 at the front):"; //remove later
     				string inputDelete;
-					cin>>inputDelete;
+					cin>>inputDelete;	
 					deleteHistory(&head, inputDelete);	//deleting
 					historyOperation=3;
 				}
@@ -72,26 +68,12 @@ int main ()
 					break;
 				}
 			}
-
+			
 		}
-
-		//to show the calculator instruction
-        else if(OnOff==3){
-            cout<<"Symbol in this calculator: \n";
-            cout <<"/: Division (/)\t\t\t"<<"s: Sin (in rad)\t\t"<<"\t\tx: square root"<<endl;
-            cout <<"*: Multiplication (*)\t\t"<<"c: Cos (in rad)\t\t"<<"\t\ty: log"<<endl;
-            cout <<"-: Subtraction (-)\t\t"<<"t: Tan (in rad)\t\t"<<"\t\tz: ln"<<endl;
-            cout <<"+: Addition (+)\t\t\t"<<"m: Inverse of Sin (in rad)\t\t"<<endl;
-            cout <<"^: Exponent (i^n))\t\t"<<"n: Inverse of Cos (in rad)\t\t"<<endl;
-            cout <<"\t\t\t\t"<<"o: Inverse of Tan (in rad)\t\t"<<endl;
-
-            cout <<"\nExample of using trigonometric function: \ns90 = sin 90 (in radian)\n\n";
-
-        }
-
+		
 		//shut down
 		else if(OnOff==0){
-			cout<<"Thank you for using the calculator";
+			cout<<"Thank you";
 			return 0;
 		}
 	}
